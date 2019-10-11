8782e775e712d1a8192b02f5166b5615002a062d
Manteliz <Manteliz@users.noreply.github.com>
2019-10-11 13:31:34 +0300

    Using StringUtils and added dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,13 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+	
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, " ");
+		return (words == null) ? 0 : separateWords.length;
+	}
     
     public Application() {
         System.out.println ("Inside Application");
@@ -8,7 +15,12 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
+    	System.out.println("Testuojam");
     	System.out.println ("Starting Application");
-	Application app = new Application();
+    	Application app = new Application();
+    	int count = app.countWords("Du zodziai");
+    	System.out.println("Zodziu skaicius: " + count);
+	
+	
     }
 }
